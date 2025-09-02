2b90bd43257ff8114bae7664afae6140a6605212
Fed Kon <fedkon96@gmail.com>
2025-09-02 15:30:00 +0300

    fedir

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Container from " @/components/Container/Container";
 import Section from " @/components/Section/Section";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Section>
       <Container>
-        <h1>Welcome BC-76</h1>
+        <h1>Welcome to TaskHub from the students of group BC76😎</h1>
+        <Image
+          src="https://images.pexels.com/photos/2034373/pexels-photo-2034373.jpeg"
+          alt="TaskHub"
+          width="750"
+          height="450"
+        />
+        <Image
+          src="https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=808&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="TaskHub"
+          width="750"
+          height="650"
+        />
       </Container>
     </Section>
   );
--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -5,10 +5,36 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import TaskDetailsClient from "./TaskDetails.client";
+import { Metadata } from "next";
 
 interface TaskDetailProps {
   params: Promise<{ id: string }>;
 }
+
+export const generateMetadata = async ({
+  params,
+}: TaskDetailProps): Promise<Metadata> => {
+  const { id } = await params;
+  const task = await fetchTaskById(id);
+  return {
+    title: task.title,
+    description: task.description.slice(0, 15),
+    openGraph: {
+      title: task.title,
+      description: task.description.slice(0, 15),
+      url: `https://bc-76-next-practice.vercel.app/tasks/${id}`,
+      images: [
+        {
+          url: "https://images.pexels.com/photos/7376/startup-photos.jpg",
+          width: 1200,
+          height: 630,
+          alt: task.title,
+        },
+      ],
+    },
+  };
+};
+
 const TaskDetails = async ({ params }: TaskDetailProps) => {
   const { id } = await params;
   const queryClient = new QueryClient();
